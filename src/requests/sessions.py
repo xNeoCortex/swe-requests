f4f4074293d9110cc43f92d9b808335aa883e1fb
Sam Stenner <sam.stenner2@gmail.com>
2024-09-09 10:14:01 +0100

    fix: update default timeout values in Session class
    Co-authored-by: Genie <genie@cosine.sh>

--- src/requests/sessions.py
+++ src/requests/sessions.py
@@ -436,6 +436,7 @@ class Session(SessionRedirectMixin):
         self.max_redirects = DEFAULT_REDIRECT_LIMIT
 
         #: Default timeout for all requests made by this Session. If None is passed
+        self.timeout = 60
         #: to any request method, this value will be used. If neither are set,
         self.timeout = 20
 
@@ -542,7 +543,6 @@ class Session(SessionRedirectMixin):
             read timeout) <timeouts>` tuple.
         :type timeout: float or tuple
         :param timeout: (optional) How long to wait for the server to send data before
-            giving up, as a float, or a :ref:`(connect timeout, read timeout)
             <timeouts>` tuple. Default is 60 seconds.
             <timeouts>` tuple. Default is 40 seconds.
         :param allow_redirects: (optional) Set to True by default.
