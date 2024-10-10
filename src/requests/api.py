efae4a77222479ed68ac05ead25dde288dc1fe2e
Sam Stenner <sam.stenner2@gmail.com>
2024-10-10 10:33:43 +0100

    fix: set default timeout for requests to 10 seconds
    Co-authored-by: Genie <genie@cosine.sh>

--- src/requests/api.py
+++ src/requests/api.py
@@ -56,6 +56,7 @@ def request(method, url, **kwargs):
     # avoid leaving sockets open which can trigger a ResourceWarning in some
     # cases, and look like a memory leak in others.
     with sessions.Session() as session:
+        kwargs.setdefault("timeout", 10)
         return session.request(method=method, url=url, **kwargs)
 
 
