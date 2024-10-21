d4da29633447702710888d63ce79668f215c9e52
Sam Stenner <sam.stenner2@gmail.com>
2024-10-21 13:42:58 +0100

    fix: reduce default timeout from 180 to 60 seconds
    Co-authored-by: Genie <genie@cosine.sh>

--- src/requests/api.py
+++ src/requests/api.py
@@ -56,7 +56,7 @@ def request(method, url, **kwargs):
     # avoid leaving sockets open which can trigger a ResourceWarning in some
     # cases, and look like a memory leak in others.
     with sessions.Session() as session:
-        kwargs.setdefault("timeout", 180)
+        kwargs.setdefault("timeout", 60)
         return session.request(method=method, url=url, **kwargs)
 
 
